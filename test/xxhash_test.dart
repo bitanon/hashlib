e67904091887e857df06d9bac9363e683799173b
Sudipto Chandra <dipu.sudipta@gmail.com>
2024-04-14 18:07:45 +0400

    Fix fixure line separator in test

--- test/xxhash_test.dart
+++ test/xxhash_test.dart
@@ -14,14 +14,19 @@ File fixtureFile(String filename) {
   return File(Directory('test/fixures/$filename').absolute.path);
 }
 
+List<String> readLines(String filename) {
+  final text = fixtureFile('long.txt').readAsStringSync();
+  return text.split(RegExp(r'(\r)?\n'));
+}
+
 void main() {
   group('xxHash test with fixures', () {
     const int maxLength = 1000;
     final text = fixtureFile('long.txt').readAsBytesSync();
 
     test('for xxh32', () {
-      var actual = fixtureFile('xxh32.csv');
-      for (var line in actual.readAsLinesSync()) {
+      var actual = readLines('xxh32.csv');
+      for (var line in actual) {
         var parts = line.split(',');
         if (parts.length != 2) continue;
         int len = int.parse(parts[0]);
@@ -35,8 +40,8 @@ void main() {
     });
 
     test('for xxh64', () {
-      var actual = fixtureFile('xxh64.csv');
-      for (var line in actual.readAsLinesSync()) {
+      var actual = readLines('xxh64.csv');
+      for (var line in actual) {
         var parts = line.split(',');
         if (parts.length != 2) continue;
         int len = int.parse(parts[0]);
@@ -50,8 +55,8 @@ void main() {
     });
 
     test('for xxh3-64', () {
-      var actual = fixtureFile('xxh3_64.csv');
-      for (var line in actual.readAsLinesSync()) {
+      var actual = readLines('xxh3_64.csv');
+      for (var line in actual) {
         var parts = line.split(',');
         if (parts.length != 2) continue;
         int len = int.parse(parts[0]);
@@ -65,8 +70,8 @@ void main() {
     });
 
     test('for xxh3-128', () {
-      var actual = fixtureFile('xxh3_128.csv');
-      for (var line in actual.readAsLinesSync()) {
+      var actual = readLines('xxh3_128.csv');
+      for (var line in actual) {
         var parts = line.split(',');
         if (parts.length != 2) continue;
         int len = int.parse(parts[0]);
