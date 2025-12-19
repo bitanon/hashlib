c7f197023ece16b2c11f69427843cdb8b64b9e72
Wu Tingfeng <wutingfeng@outlook.com>
2025-12-19 21:55:57 +0800

    Use process.versions.node.

--- lib/src/random/generator_js.dart
+++ lib/src/random/generator_js.dart
@@ -1,10 +1,18 @@
 // Copyright (c) 2024, Sudipto Chandra
 // All rights reserved. Check LICENSE file for details.
 
+import 'dart:js_interop_unsafe';
 import 'dart:math' show Random;
 import 'dart:js_interop';
 
 const int _mask32 = 0xFFFFFFFF;
+
+@JS('process')
+external JSObject? get _process;
+
+bool get _isNodeJS => ((_process?.getProperty('versions'.toJS) as JSObject?)
+        ?.getProperty('node'.toJS))
+    .isDefinedAndNotNull;
 
 @JS()
 @staticInterop
@@ -17,17 +25,8 @@ extension on Crypto {
 @JS()
 external Crypto require(final String id);
 
+/// For Node.js environment + dart2js compiler
 class NodeRandom implements Random {
-  @override
-  bool nextBool() => nextInt(2) == 1;
-
-  @override
-  double nextDouble() {
-    final int a = nextInt(1 << 26); // 26 bits
-    final int b = nextInt(1 << 27); // 27 bits
-    return ((a << 27) + b) / (1 << 53); // 26 + 27 = 53 bits (JS limit)
-  }
-
   @override
   int nextInt(final int max) {
     if (max < 1 || max > _mask32 + 1) {
@@ -36,21 +35,21 @@ class NodeRandom implements Random {
     }
     return require('crypto').randomInt(max);
   }
+
+  @override
+  double nextDouble() {
+    final int first26Bits = nextInt(1 << 26);
+    final int next27Bits = nextInt(1 << 27);
+    final int random53Bits = (first26Bits << 27) + next27Bits; // JS int limit
+    return random53Bits / (1 << 53);
+  }
+
+  @override
+  bool nextBool() => nextInt(2) == 1;
 }
 
 /// Returns a secure random generator in JS runtime
-Random secureRandom() {
-  try {
-    return Random.secure();
-  } catch (e) {
-    // For Node.js with dart2js compiler, the following error is expected.
-    if (e.runtimeType.toString() == 'UnknownJsTypeError') {
-      // This error is internal to 'dart:_js_helper'.
-      return NodeRandom();
-    }
-    rethrow;
-  }
-}
+Random secureRandom() => _isNodeJS ? NodeRandom() : Random.secure();
 
 /// Generates a random seed
 int $generateSeed() => secureRandom().nextInt(_mask32);
